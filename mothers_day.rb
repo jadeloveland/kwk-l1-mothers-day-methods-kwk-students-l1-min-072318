78579a9359dba4541d54b52333f9b8d92259caf0
jadeloveland <jloveland93@gmail.com>
2018-07-24 16:15:54 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,5 +1,6 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-def mothers_day(defalt = "Mom!")
-  "Happy Mother's Day, #{de"
+def mothers_day(default = "Mom!")
+  "Happy Mother's Day, #{default}"
 end
 
+mothers_day("Beyonce")
